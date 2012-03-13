c396b43b38e6d3cfbb059696a78736d71cb8f628
Hendy Irawan <hendy@soluvas.com>
2012-03-13 11:38:29 +0700

    ruby and augeas

--- prepare-puppet.sh
+++ prepare-puppet.sh
@@ -6,10 +6,10 @@ fi
 
 # Use apt-get since aptitude is not available in Ubuntu minimal VM
 apt-get update
-apt-get install -y ruby1.9.1-full augeas-lenses git
+apt-get install -y ruby1.9.1-dev augeas-lenses libaugeas-dev git
 ln -sv gem1.9.1 /usr/bin/gem
 REALLY_GEM_UPDATE_SYSTEM=1 gem update --system
-gem install -V puppet facter
+gem install -V puppet facter ruby-augeas
 
 mkdir -vp /etc/puppet/modules /etc/puppet/manifests
 adduser --system --group --home /etc/puppet --no-create-home --disabled-password puppet
